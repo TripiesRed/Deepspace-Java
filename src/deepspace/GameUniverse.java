fd0b31f83f83b589857d289e36af74dc01c492b8
diegoos03 <diorsa03@gmail.com>
2023-04-29 20:58:18 +0200

    P2

--- src/deepspace/GameUniverse.java
+++ src/deepspace/GameUniverse.java
@@ -3,13 +3,125 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package deepspace;
+import java.util.ArrayList;
 
 /**
  *
  * @author gonzalomp
  */
 public class GameUniverse {
+    private static final int WIN = 10;
+    private int currentStationIndex;
+    private int turns;
+    private GameStateController gameState;
+    private Dice dice;
+    private SpaceStation currentStation;
+    private ArrayList<SpaceStation> spaceStations;
+    private EnemyStarShip currentEnemy;
     
     
+    GameUniverse(){
+        gameState = new GameStateController();
+        turns = 0;
+        dice = new Dice();
+        currentEnemy = null;
+        spaceStations =  new ArrayList<SpaceStation>();
+        currentStationIndex = 0;
+        currentStation = null;
+    }
     
+    void mountShiedBooster(int i){
+        
+        if(gameState.getState() == GameState.INIT || 
+           gameState.getState() == GameState.AFTERCOMBAT){
+            currentStation.mountShieldBooster(i);
+        }
+    }
+    
+    void mountWeapon(int i){
+        
+        if(gameState.getState() == GameState.INIT || 
+           gameState.getState() == GameState.AFTERCOMBAT){
+            currentStation.mountWeapon(i);
+        }
+    }
+    
+    void discardHangar(){
+        
+        if(gameState.getState() == GameState.INIT || 
+           gameState.getState() == GameState.AFTERCOMBAT){
+            currentStation.discardHangar();
+        }
+    }
+    
+    void discardShieldBooster(int i){
+        
+        if(gameState.getState() == GameState.INIT || 
+           gameState.getState() == GameState.AFTERCOMBAT){
+            currentStation.discardShieldBooster(i);
+        }
+    }
+    
+    void discardShieldBoosterInHangar(int i){
+        
+        if(gameState.getState() == GameState.INIT || 
+           gameState.getState() == GameState.AFTERCOMBAT){
+            currentStation.discardShieldBoosterInHangar(i);
+        }
+    }
+    
+    void discardWeapon(int i){
+        
+        if(gameState.getState() == GameState.INIT || 
+           gameState.getState() == GameState.AFTERCOMBAT){
+            currentStation.discardWeapon(i);
+        }
+    }
+    
+    void discardWeaponInHangar(int i){
+        
+        if(gameState.getState() == GameState.INIT || 
+           gameState.getState() == GameState.AFTERCOMBAT){
+            currentStation.discardWeaponInHangar(i);
+        }
+    }
+    
+    boolean haveAWinner(){
+        
+        if(currentStation.getNMedals() == WIN){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    
+    boolean nextTurn(){
+        
+        if(gameState.getState() == GameState.AFTERCOMBAT){
+            
+            if(currentStation.validState()){
+                
+                currentStationIndex = (currentStationIndex + 1) 
+                                    % spaceStations.size();
+                turns++;
+                currentStation = spaceStations.get(currentStationIndex);
+                currentStation.cleanUpMountedItems();
+                
+                CardDealer dealer = CardDealer.getInstance();
+                
+                currentEnemy = dealer.nextEnemy();
+                gameState.next(turns, spaceStations.size());
+                
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        else{
+                   
+            return false;
+        }
+    }
 }
