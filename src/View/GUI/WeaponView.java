b503c1a1a2a838cd88dba1cbbc7bbb77a98425ad
diegoos03 <diorsa03@gmail.com>
2023-05-29 19:53:14 +0200

    Algunas vistas

--- src/View/GUI/WeaponView.java
+++ src/View/GUI/WeaponView.java
@@ -14,14 +14,24 @@ public class WeaponView extends javax.swing.JPanel {
     /**
      * Creates new form WeaponView
      */
-    public WeaponView(WeaponToUI w) {
+    public WeaponView() {
         initComponents(); 
         
-        jLabel1.setText("Tipo: " + w.getType().toString());
-        jLabel2.setText("Potencia: " + Float.toString(w.getPower()));
-        jLabel3.setText("Usos: " + Integer.toString(w.getUses()));
+        /*
+        Tipo.setText("Tipo: " + w.getType().toString());
+        Potencia.setText("Potencia: " + Float.toString(w.getPower()));
+        Usos.setText("Usos: " + Integer.toString(w.getUses()));
+        */
     }
-
+    
+    public void setWeaponView(WeaponToUI w){
+        
+        Tipo.setText("Tipo: " + w.getType().toString());
+        Potencia.setText("Potencia: " + Float.toString(w.getPower()));
+        Usos.setText("Usos: " + Integer.toString(w.getUses()));
+        repaint();
+    }
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -31,19 +41,19 @@ public class WeaponView extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
+        Tipo = new javax.swing.JLabel();
+        Potencia = new javax.swing.JLabel();
+        Usos = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
 
         setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 0, 0), 1, true));
         setMaximumSize(new java.awt.Dimension(200, 103));
 
-        jLabel1.setText("Tipo:");
+        Tipo.setText("Tipo:");
 
-        jLabel2.setText("Potencia:");
+        Potencia.setText("Potencia:");
 
-        jLabel3.setText("Usos:");
+        Usos.setText("Usos:");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -52,10 +62,10 @@ public class WeaponView extends javax.swing.JPanel {
             .addGroup(layout.createSequentialGroup()
                 .addGap(34, 34, 34)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel3)
-                    .addComponent(jLabel2)
+                    .addComponent(Usos)
+                    .addComponent(Potencia)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel1)
+                        .addComponent(Tipo)
                         .addGap(18, 18, 18)
                         .addComponent(jLabel4)))
                 .addContainerGap(88, Short.MAX_VALUE))
@@ -65,21 +75,21 @@ public class WeaponView extends javax.swing.JPanel {
             .addGroup(layout.createSequentialGroup()
                 .addGap(24, 24, 24)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel1)
+                    .addComponent(Tipo)
                     .addComponent(jLabel4))
                 .addGap(18, 18, 18)
-                .addComponent(jLabel2)
+                .addComponent(Potencia)
                 .addGap(18, 18, 18)
-                .addComponent(jLabel3)
+                .addComponent(Usos)
                 .addContainerGap(20, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel Potencia;
+    private javax.swing.JLabel Tipo;
+    private javax.swing.JLabel Usos;
     private javax.swing.JLabel jLabel4;
     // End of variables declaration//GEN-END:variables
 }
